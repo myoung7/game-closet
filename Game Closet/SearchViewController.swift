f87daecc8da695dd4ec077771f9a74baea6b0e5f
Matthew Young <myoung7@users.noreply.github.com>
2016-04-03 23:11:38 -0700

    First commit

--- Game Closet/SearchViewController.swift
+++ Game Closet/SearchViewController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  Game Closet
 //
 //  Created by Matthew Young on 4/3/16.
 //  Copyright © 2016 Matthew Young. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
